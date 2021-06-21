e522bb00782d64f1874427619112b17a84fc93e7
Joao Pedro Renan <jota.renan@gmail.com>
2021-06-21 13:22:53 -0300

    initial

---
+++ app2/dist/declarations/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+declare module "components/Button" {
+    const Button: () => JSX.Element;
+    export default Button;
+}
+declare module "App" {
+    const App: () => JSX.Element;
+    export default App;
+}
+declare module "bootstrap" { }
+declare module "index" { }
